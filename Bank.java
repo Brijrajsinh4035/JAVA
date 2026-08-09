4f2332cc73bcbd2e4717b0ebb98d2a4c4830dfb1
Brijrajsinh4035 <brijrajsinh4035@gmail.com>
2026-08-09 11:44:04 +0530

    Add some static class and method

--- Bank.java
+++ Bank.java
@@ -1,22 +1,27 @@
 import java.util.*;
 
-class BankAdd{
+class BankAdd {
     private double Balance;
     public int AccountNo;
-    
-    BankAdd(double Balances,int ANo){
+    static String BankName = "Kotak Bank";
+    static double rate;
+    private int ATMPin;
+
+    static {
+        BankName = "hdfc bank";
+    }
+
+    BankAdd(double Balances, int ANo) {
         Balance = Balances;
         AccountNo = ANo;
     }
 
-    BankAdd(BankAdd B)
-    {
+    BankAdd(BankAdd B) {
         Balance = B.Balance;
         AccountNo = B.AccountNo;
     }
 
-    public void BankProcess(Scanner sc)
-    {
+    public void BankProcess(Scanner sc) {
         while (true) {
             System.out.println("1. See Account Details");
             System.out.println("2. Deposit");
@@ -25,55 +30,78 @@ class BankAdd{
             int n = sc.nextInt();
             switch (n) {
                 case 1:
-                    System.out.println("Account No: "+AccountNo);
-                    System.out.println("Balance: "+Balance);
+                    System.out.println("Bank Name: " + BankName);
+                    System.out.println("Account No: " + AccountNo);
+                    System.out.println("Balance: " + Balance);
                     break;
                 case 2:
                     System.out.println("Enter Deposit Amount");
                     double D = sc.nextDouble();
                     Balance += D;
-                    System.out.println("Total Balance: "+Balance);
-                    break;    
+                    System.out.println("Total Balance: " + Balance);
+                    break;
                 case 3:
                     System.out.println("Enter Amount For Withdraw");
                     double w = sc.nextDouble();
-                    if(Balance-w < 10000)
-                    {
+                    if (Balance - w < 10000) {
                         System.out.println("Rs 10000 Minimum Balance is required Withdraw is Rejected");
-                    }
-                    else
-                    {
-                    Balance -= w;
-                    System.out.println("Total Balance: "+Balance);
+                    } else {
+                        Balance -= w;
+                        System.out.println("Total Balance: " + Balance);
                     }
                     break;
                 case 4:
                     return;
-                
+
                 default:
                     System.out.println("Enter Valid Process Number");
                     break;
             }
         }
     }
+
+    public static void setrate(Scanner sc) {
+        System.out.println("Enter Interes Rate");
+        rate = sc.nextDouble();
+    }
+
+    public static void getrate() {
+        System.out.println("Interes Rate: " + rate);
+    }
+
+    class ATMPin {
+    
+        public void SetPin(Scanner sc) {
+            System.out.println("enter Atm pin");
+            ATMPin = sc.nextInt();
+        }
+
+        public void GetPin() {
+            System.out.println("ATM PIN: " + ATMPin);
+        }
+
+    }
 }
 
 public class Bank {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Account Number");
         int AccountNo = sc.nextInt();
         System.out.println("Enter Balances (Minimum 10,000)");
         double Balances = sc.nextDouble();
-        if(Balances<10000)
-        {
+        if (Balances < 10000) {
             System.out.println("Balance Should Minimum 10,000");
             System.exit(0);
         }
         BankAdd B1 = new BankAdd(Balances, AccountNo);
-        BankAdd B2 = new BankAdd(B1);
+        //BankAdd B2 = new BankAdd(B1);
+        BankAdd.ATMPin A1 = B1.new ATMPin();
+        A1.SetPin(sc);
+        A1.GetPin();
+        BankAdd.setrate(sc);
+        BankAdd.getrate();
         B1.BankProcess(sc);
-        B2.BankProcess(sc);
-    }    
+        //B2.BankProcess(sc);
+    }
 }
